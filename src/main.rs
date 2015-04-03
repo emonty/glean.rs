b8f4cf1d3dae363876725aed6e5c28faad16a89d
Monty Taylor <mordred@inaugust.com>
2015-04-03 13:46:01 -0400

    Implement custom Decodable for Service and Network

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 #![feature(custom_derive)]
 extern crate rustc_serialize;
-use rustc_serialize::json;
+use rustc_serialize::{json, Decodable, Decoder};
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::env;
 
-// Automatically generate `RustcDecodable` and `RustcEncodable` trait
+// Automatically generate `Decodable` and `Encodable` trait
 // implementations
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcEncodable, Debug)]
 pub struct Service {
     service_type: String,
     address: String,
 }
+
+impl Decodable for Service {
+  fn decode<D: Decoder>(decoder: &mut D) -> Result<Service, D::Error> {
+    decoder.read_struct("root", 0, |decoder| {
+         Ok(Service{
+          service_type: try!(decoder.read_struct_field("type", 0, |decoder| Decodable::decode(decoder))),
+          address: try!(decoder.read_struct_field("address", 0, |decoder| Decodable::decode(decoder)))
+        })
+    })
+  }
+}
+
 
 #[derive(RustcDecodable, RustcEncodable, Debug)]
 pub struct Route {
@@ -23,7 +35,7 @@ pub struct Route {
     gateway: String,
 }
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcEncodable, Debug)]
 pub struct Network {
     network_id: String,
     network_type: String,
@@ -32,6 +44,23 @@ pub struct Network {
     routes: Vec<Route>,
     ip_address: String,
     id: String,
+}
+
+// Custom class because type can't be a struct member
+impl Decodable for Network {
+  fn decode<D: Decoder>(decoder: &mut D) -> Result<Network, D::Error> {
+    decoder.read_struct("root", 0, |decoder| {
+         Ok(Network{
+          network_id: try!(decoder.read_struct_field("network_id", 0, |decoder| Decodable::decode(decoder))),
+          network_type: try!(decoder.read_struct_field("type", 0, |decoder| Decodable::decode(decoder))),
+          netmask: try!(decoder.read_struct_field("netmask", 0, |decoder| Decodable::decode(decoder))),
+          link: try!(decoder.read_struct_field("link", 0, |decoder| Decodable::decode(decoder))),
+          routes: try!(decoder.read_struct_field("routes", 0, |decoder| Decodable::decode(decoder))),
+          ip_address: try!(decoder.read_struct_field("ip_address", 0, |decoder| Decodable::decode(decoder))),
+          id: try!(decoder.read_struct_field("id", 0, |decoder| Decodable::decode(decoder))),
+        })
+    })
+  }
 }
 
 #[derive(RustcDecodable, RustcEncodable, Debug)]
